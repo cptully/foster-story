c1613597cd7512c91af4a794949f097869570ada
Chris Tully <cptully@gmail.com>
2016-10-15 21:13:05 -0400

    add PostService and add method to get Tumblr posts from all blogs with a given tag

--- src/main/java/com/fosterstory/repository/PostRepository.java
+++ src/main/java/com/fosterstory/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.fosterstory.repository;
 
 import com.fosterstory.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer>{
+    Page<Post> findAll(Pageable pageable);
 }
